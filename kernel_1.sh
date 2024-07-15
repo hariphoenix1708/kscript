db058546a4e70858ae17e96ba4f00f3edd2c15dc
hariphoenix1708 <138122182+hariphoenix1708@users.noreply.github.com>
2024-07-15 10:48:53 +0530

    Update kernel_1.sh

--- kernel_1.sh
+++ kernel_1.sh
@@ -172,7 +172,7 @@ tg_error() {
 
 build_kernel() {
 Start=$(date +"%s")
-	make "$DEFCONFIG_DEVICE" O=out
+	make  O=out "$DEFCONFIG_DEVICE"
 
 	make -j$(nproc --all) O=out \
                               ARCH=arm64 \
